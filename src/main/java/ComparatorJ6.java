76717befe1043a09e32d0a3cc9c6e95eedf59f49
Andrzej Miklas <atmiklas@gmail.com>
2017-11-01 17:52:16 +0100

    Gradle

---
+++ src/main/java/ComparatorJ6.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by jedrus on 10/31/2017.
+ */
+public class ComparatorJ6 {
+    public static void main(String[] args) {
+        sortByImperativeComparator();
+        sortByLambdaComparator();
+    }
+
+    private static void sortByLambdaComparator() {
+        List<String> list = Arrays.asList(new String[] {"B", "A"});
+
+        //
+        // Przekazanie kodu jako parametru
+
+        Collections.sort(list, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+    }
+
+
+    private static void sortByImperativeComparator() {
+        List<String> list = Arrays.asList(new String[] {"B", "A"});
+
+        Comparator c = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        };
+
+        Collections.sort(list, c);
+    }
+}
